1b7a69fd73ec4d9076422664506a1e3793cc86bf
paras41617 <paras41617@gmail.com>
2024-10-11 20:47:46 +0530

    refactor: correcting the env variable

--- app/__init__.py
+++ app/__init__.py
@@ -18,8 +18,8 @@ db.init_app(app)
 cloudinary.config(
     cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
     api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARU_API_SECRET'),
-    secure=os.getenv('CLOUDINARU_SECURE')
+    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    secure=os.getenv('CLOUDINARY_SECURE')
 )
 
 
